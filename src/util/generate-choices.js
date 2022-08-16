17463592cb4419469d2b81517f88a8b437d385b6
Dillon Schultz <dillonschultz93@gmail.com>
2022-08-16 13:14:05 -0700

    Refactor

--- src/util/generate-choices.js
+++ src/util/generate-choices.js
@@ -1,64 +1,33 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
 const path = require('path');
-
+const _ = require('lodash');
 /**
  * Generate choice tokens from the super tokens.
  * @param {*} superTokensObject - The super tokens object.
  */
-const generateTokens = (superTokensObject) => {
-  const { projects } = superTokensObject;
-  // const { nomenclatureOptions } = configuration;
+const generateChoices = (superTokensObject, superTokenCategory) => {
+  const choices = [];
 
-  const tokens = [];
+  Object.entries(superTokensObject).forEach(([category, value]) => {
+    const { apparatusProtocol, tokenset, options, kingdom } = value;
+    const apparatus = require(path.join(__dirname, '../apparatuses', apparatusProtocol));
 
-  projects.forEach((project) => {
-    const { superChoices, name } = project;
+    const choiceSet = {
+      [kingdom]: {
+        [superTokenCategory]: {
+          [category]: {
+            tokenset,
+            ...apparatus(options),
+          },
+        },
+      },
+    };
 
-    Object.entries(superChoices).forEach(([superChoiceCategory, superChoiceCategoryValue]) => {
-      let categoryTokens = {};
-
-      Object.entries(superChoiceCategoryValue).forEach(([choiceCategory, config]) => {
-        const { apparatusProtocol, options, kingdom, tokenset } = config;
-
-        if (apparatusProtocol === null) {
-          // const key = `${nomenclatureOptions.namespace}`;
-          const tokenSet = {
-            [kingdom]: {
-              [superChoiceCategory]: {
-                [choiceCategory]: {
-                  tokenset,
-                  ...structuredClone(options),
-                },
-              },
-            },
-          };
-
-          categoryTokens = deepMerge(tokenSet, {});
-        } else {
-          const apparatus = require(path.join(__dirname, '../apparatuses', apparatusProtocol));
-
-          // const key = `${nomenclatureOptions.namespace}`;
-          const tokenSet = {
-            [kingdom]: {
-              [superChoiceCategory]: {
-                [choiceCategory]: {
-                  tokenset,
-                  ...apparatus(options),
-                },
-              },
-            },
-          };
-
-          categoryTokens = deepMerge(tokenSet, {});
-        }
-
-        tokens.push({ project: name, parent: superChoiceCategory, category: choiceCategory, tokens: categoryTokens });
-      });
-    });
+    choices.push({ parent: superTokenCategory, category, tokens: _.merge(choiceSet, {}) });
   });
 
-  return tokens;
+  return choices;
 };
 
-module.exports = generateTokens;
+module.exports = generateChoices;
