e589b8b455e568efd337d3c7a504b600a420fdcd
Dillon Schultz <dillonschultz93@gmail.com>
2022-08-16 13:36:47 -0700

    Rename file

---
+++ src/util/write-choices.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const writeChoices = (choices) => {
+  choices.forEach((choice) => {
+    const { parent, category, tokens } = choice;
+
+    fs.mkdirSync(path.join(__dirname, `../src/tokens/choices/${parent}`), { recursive: true });
+
+    fs.writeFileSync(
+      path.join(__dirname, `../src/tokens/choices/${parent}/${category}.tokens.json5`),
+      JSON.stringify(tokens, null, 2),
+      'utf-8'
+    );
+  });
+};
+
+module.exports = writeChoices;
