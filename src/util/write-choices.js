fde484cf6af3b756f7c58b9ec779ddd6cba554f7
Dillon Schultz <dillonschultz93@gmail.com>
2022-08-17 10:51:16 -0700

    Fix relative path

--- src/util/write-choices.js
+++ src/util/write-choices.js
@@ -5,10 +5,10 @@ const writeChoices = (choices) => {
   choices.forEach((choice) => {
     const { parent, category, tokens } = choice;
 
-    fs.mkdirSync(path.join(__dirname, `../src/tokens/choices/${parent}`), { recursive: true });
+    fs.mkdirSync(path.join(__dirname, `../../src/tokens/choices/${parent}`), { recursive: true });
 
     fs.writeFileSync(
-      path.join(__dirname, `../src/tokens/choices/${parent}/${category}.tokens.json5`),
+      path.join(__dirname, `../../src/tokens/choices/${parent}/${category}.tokens.json5`),
       JSON.stringify(tokens, null, 2),
       'utf-8'
     );
