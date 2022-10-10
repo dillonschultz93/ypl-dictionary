7095dca72f7f652b2c1dcac8641c01add73f34af
Dillon Schultz <dillonschultz93@gmail.com>
2022-10-10 09:52:26 -0700

    Comment for clarity

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const getTokens = () => tokens;
 
 const getNestedTokens = (tokenset, useArrays) => unflatten(tokenset, { object: !useArrays });
 
+// TODO: This is where we will call the dictionary scripts.
+
 module.exports = {
   getTokens,
   getValueFromFlatTokens,
