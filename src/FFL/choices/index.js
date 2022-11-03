33f19e39ecfc880936455606e013dc22425417f0
Dillon Schultz <dillonschultz93@gmail.com>
2022-11-03 16:40:41 -0700

    Import the new constant choices

--- src/FFL/choices/index.js
+++ src/FFL/choices/index.js
@@ -1,7 +1,9 @@
-const getRequiredAndFlattenedFiles = require('../../util/getRequiredAndFlattenedFiles');
+const getRequiredAndFlattenedFiles = require('../../_util/getRequiredAndFlattenedFiles');
 const TKUI_A = require('./TKUI_A');
 const TKUI_C = require('./TKUI_C');
 const TKUI_M = require('./TKUI_M');
+// Importing in global constants
+const CONST = require('../../_constants');
 
 // Get any files in the current directory, require and flatten them
 const currentDir = getRequiredAndFlattenedFiles(__dirname);
@@ -11,4 +13,5 @@ module.exports = {
   ...TKUI_C,
   ...currentDir, // Any files in the current directory
   ...TKUI_M,
+  ...CONST, // Global constants
 };
