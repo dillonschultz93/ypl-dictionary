b178debb84beb3465534d4a6cd52f4473ec477c6
Dillon Schultz <dillonschultz93@gmail.com>
2022-11-18 10:55:34 -0700

    Add Status element to the barrel export

--- src/FFL/decisions/elements/index.js
+++ src/FFL/decisions/elements/index.js
@@ -1,16 +1,19 @@
-const Chip = require('./E0001-Chip');
-const Thumbnail = require('./E0002-Thumbnail');
-const Switch = require('./E0004-Switch');
+const Chip = require('./E0001-Chip'); // E0001
+const Thumbnail = require('./E0002-Thumbnail'); // E0002
+const Switch = require('./E0004-Switch'); // E0004
+const Status = require('./E0005-Status'); // E0005
 
 module.exports = {
   allElementTokens: {
     ...Chip.tokens,
     ...Thumbnail.tokens,
     ...Switch.tokens,
+    ...Status.tokens,
   },
   allElementKBInfo: {
     ...Chip.kbInfo,
     ...Thumbnail.kbInfo,
     ...Switch.kbInfo,
+    ...Status.kbInfo,
   },
 };
