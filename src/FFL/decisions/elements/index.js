c110272c20145a774c57d6c69062a7ebe053e506
Dillon Schultz <dillonschultz93@gmail.com>
2022-11-17 17:25:18 -0700

    Add Thumbnail to the barrel exports

--- src/FFL/decisions/elements/index.js
+++ src/FFL/decisions/elements/index.js
@@ -1,13 +1,16 @@
 const Chip = require('./E0001-Chip');
+const Thumbnail = require('./E0002-Thumbnail');
 const Switch = require('./E0004-Switch');
 
 module.exports = {
   allElementTokens: {
     ...Chip.tokens,
+    ...Thumbnail.tokens,
     ...Switch.tokens,
   },
   allElementKBInfo: {
     ...Chip.kbInfo,
+    ...Thumbnail.kbInfo,
     ...Switch.kbInfo,
   },
 };
