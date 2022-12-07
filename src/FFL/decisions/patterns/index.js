4e9c206013c26341f0a2edeb7c448770c550c09a
Dillon Schultz <dillonschultz93@gmail.com>
2022-12-06 19:55:07 -0700

    Create Lockup Col pattern

--- src/FFL/decisions/patterns/index.js
+++ src/FFL/decisions/patterns/index.js
@@ -1,10 +1,13 @@
 const PosterCard = require('./P0001-Poster Card');
+const LockupCol = require('./P0003-Lockup Col');
 
 module.exports = {
   allPatternTokens: {
     ...PosterCard.tokens,
+    ...LockupCol.tokens,
   },
   allPatternKBInfo: {
     ...PosterCard.kbInfo,
+    ...LockupCol.kbInfo,
   },
 };
