c028e53d8878d7c9ae16c25738957dce93c4a2a7
Dillon Schultz <dillonschultz93@gmail.com>
2022-12-08 19:03:53 -0700

    Add cardlet to barrel export

--- src/FFL/decisions/patterns/index.js
+++ src/FFL/decisions/patterns/index.js
@@ -1,4 +1,5 @@
 const PosterCard = require('./P0001-Poster Card');
+const Cardlet = require('./P0002-Cardlet');
 const LockupCol = require('./P0003-Lockup Col');
 const FlashCard = require('./P0007-Flash Card');
 const Navigation = require('./P0009-Navigation');
@@ -6,12 +7,14 @@ const Navigation = require('./P0009-Navigation');
 module.exports = {
   allPatternTokens: {
     ...PosterCard.tokens,
+    ...Cardlet.tokens,
     ...LockupCol.tokens,
     ...FlashCard.tokens,
     ...Navigation.tokens,
   },
   allPatternKBInfo: {
     ...PosterCard.kbInfo,
+    ...Cardlet.kbInfo,
     ...LockupCol.kbInfo,
     ...FlashCard.kbInfo,
     ...Navigation.kbInfo,
