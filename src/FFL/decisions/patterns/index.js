44fca236e0a933fe699f6c48694739d9e90febf9
Dillon Schultz <dillonschultz93@gmail.com>
2022-12-08 18:53:55 -0700

    Create missing constants

--- src/FFL/decisions/patterns/index.js
+++ src/FFL/decisions/patterns/index.js
@@ -1,16 +1,19 @@
 const PosterCard = require('./P0001-Poster Card');
 const LockupCol = require('./P0003-Lockup Col');
 const FlashCard = require('./P0007-Flash Card');
+const Navigation = require('./P0009-Navigation');
 
 module.exports = {
   allPatternTokens: {
     ...PosterCard.tokens,
     ...LockupCol.tokens,
     ...FlashCard.tokens,
+    ...Navigation.tokens,
   },
   allPatternKBInfo: {
     ...PosterCard.kbInfo,
     ...LockupCol.kbInfo,
     ...FlashCard.kbInfo,
+    ...Navigation.kbInfo,
   },
 };
