ef0bf3372f0ad3f47c3a099f5ad3540bc0d3cff6
Dillon Schultz <dillonschultz93@gmail.com>
2022-12-15 10:19:10 -0700

    Create new release

--- src/FFL/decisions/patterns/index.js
+++ src/FFL/decisions/patterns/index.js
@@ -7,6 +7,7 @@ const Header = require('./P000A-Header');
 const Toast = require('./P000B-Toast');
 const ActionBar = require('./P000E-Action Bar');
 const TabBar = require('./P000F-Tab Bar');
+const ContentSlider = require('./P0010-Content Slider');
 const Cluster = require('./P0012-Cluster');
 const Tabs = require('./P0013-Tabs');
 const Instruction = require('./P001B-Instruction');
@@ -24,6 +25,7 @@ module.exports = {
     ...Toast.tokens,
     ...ActionBar.tokens,
     ...TabBar.tokens,
+    ...ContentSlider.tokens,
     ...Cluster.tokens,
     ...Tabs.tokens,
     ...Instruction.tokens,
@@ -40,6 +42,7 @@ module.exports = {
     ...Toast.kbInfo,
     ...ActionBar.kbInfo,
     ...TabBar.kbInfo,
+    ...ContentSlider.kbInfo,
     ...Cluster.kbInfo,
     ...Tabs.kbInfo,
     ...Instruction.kbInfo,
