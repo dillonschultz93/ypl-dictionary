47d1caeb067c487ac08a93cf51758eb9182fbeaf
Dillon Schultz <dillonschultz93@gmail.com>
2022-12-19 13:55:45 -0700

    Create another new release...this time WITH Rating Widget and Item

--- src/FFL/decisions/patterns/index.js
+++ src/FFL/decisions/patterns/index.js
@@ -8,10 +8,12 @@ const Toast = require('./P000B-Toast');
 const ActionBar = require('./P000E-Action Bar');
 const TabBar = require('./P000F-Tab Bar');
 const ContentSlider = require('./P0010-Content Slider');
+const List = require('./P0011-List');
 const Cluster = require('./P0012-Cluster');
 const Tabs = require('./P0013-Tabs');
 const ListItem = require('./P0015-List Item');
 const Instruction = require('./P001B-Instruction');
+const RatingWidget = require('./P001D-Rating Widget');
 const WidgetCard = require('./P001F-Widget Card');
 const StickyAction = require('./P0020-Sticky Action');
 
@@ -27,10 +29,12 @@ module.exports = {
     ...ActionBar.tokens,
     ...TabBar.tokens,
     ...ContentSlider.tokens,
+    ...List.tokens,
     ...Cluster.tokens,
     ...Tabs.tokens,
     ...ListItem.tokens,
     ...Instruction.tokens,
+    ...RatingWidget.tokens,
     ...WidgetCard.tokens,
     ...StickyAction.tokens,
   },
@@ -45,10 +49,12 @@ module.exports = {
     ...ActionBar.kbInfo,
     ...TabBar.kbInfo,
     ...ContentSlider.kbInfo,
+    ...List.kbInfo,
     ...Cluster.kbInfo,
     ...Tabs.kbInfo,
     ...ListItem.kbInfo,
     ...Instruction.kbInfo,
+    ...RatingWidget.kbInfo,
     ...WidgetCard.kbInfo,
     ...StickyAction.kbInfo,
   },
